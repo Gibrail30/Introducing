f3a42db5a0d43f3aa8fd92a04133f543c6efcf06
Gibrail30 <ednlandergibrail@gmail.com>
2021-11-16 12:34:31 +0700

    first commit

---
+++ Gibrail_212410102024.py
@@ -0,0 +1,11 @@
+nama = "Gibrail Ahmadace'ary Ednlander"
+nim = 212410102024
+umur = 19
+tinggi = '160 (Terakhir saya periksa)'
+berat = '53 (Terakhir saya periksa)'
+
+print('Nama: ', nama)
+print('NIM: ', nim)
+print('Umur: ', umur)
+print('Tinggi badan: ', tinggi)
+print('Berat badan: ', berat)
